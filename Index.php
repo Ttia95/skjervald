787e566f71c66340d2f4bbf1e1ea5b6ecb44951d
Tatango Teteia <57804368+Ttia95@users.noreply.github.com>
2020-07-13 12:18:06 +0200

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en">
+<!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="Hospedagem Web Brasil, Revenda de hospedagem, Registo de Domínios">
+    <meta name="author" content="MyInfinit">
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <title>Hospedagem web</title>
+    <!--REQUIRED STYLE SHEETS-->
+    <!-- BOOTSTRAP CORE STYLE CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONTAWESOME STYLE CSS -->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+       <!-- CUSTOM STYLE CSS -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+      <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, inicial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/pvw-style.css">
+  <link rel="stylesheet" type="text/css" href="css/font-awesome/css/font-awesome.css">
+  <link rel="icon" href="img/logo.jpg">
+   </head>
+
+<body>
+     <!-- NAV SECTION -->
+         <div class="navbar navbar-inverse navbar-fixed-top">
+       
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a style="color: #000066;" class="navbar-brand" href="#">Hospedagem Web</a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#home-sec">INÍCIO</a></li>
+                    <li><a href="#lgrad">Planos</a></li>
+                     <li><a href="#project">Dominios</a></li>
+                     <li><a href="#contact">Contacto</a></li>
+                </ul>
+            </div>
+           
+        </div>
+    </div>
+     <!--END NAV SECTION -->
+    <!-- BANNER -->
+      <div id="banner">
+        <div id="b-all">
+          <div id="b-content">
+            <div class="b-header">
+              <h2>Don't waste your time!</h2>
+              <div class="clock"><span class="side"></span><img src="img/13.png"/><span class="side"></span></div>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip. <span class="none">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- END BANNER -->
+
+      <h1 class="ttl">Plamos de Hospedagem</h1>
+      <div id="lgrad">
+        <p class="gradient liner"></p>
+        <p class="gradient liner"></p>
+        <p class="gradient liner"></p>
+        <p class="gradient liner"></p>
+        <p class="gradient liner"></p>
+        <p class="gradient liner"></p><!-- 
+        <p class="gradient liner"></p>
+        <p class="gradient liner"></p>
+        <p class="gradient liner"></p>
+        <p class="gradient liner"></p> -->
+      </div>
+
+      <div id="middle">
+
+        <div id="two">
+        <div class="organize">
+          <div class="tres">
+            <div class="titulo">
+              <img src="img/pg.png" class="img1">
+              <h1>PHOTOGRAPHY</h1>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur.
+            </p>
+            <div>
+              <img src="img/6.jpg" class="rend-img">
+            </div>
+            <p><a href="#">See More</a></p>
+          </div>
+        </div>
+
+        <div class="organize">
+          <div class="tres">
+            <div class="titulo">
+              <img src="img/wd1.png" class="img1">
+              <h1>WEBDESIGN</h1>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur.
+            </p>
+            <div>
+              <img src="img/1.jpg" class="rend-img">
+            </div>
+            <p><a href="#">See More</a></p>
+          </div>
+        </div>
+        </div>
+
+        <div class="organize phot">
+          <div class="tres">
+            <div class="titulo">
+              <img src="img/WH.png" class="img1">
+              <h1>Elite</h1>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur.
+            </p>
+            <div>
+              <img src="img/WH.png" class="rend-img">
+            </div>
+            <p><a href="#">See More</a></p>
+          </div>
+        </div>
+
+        <div class="organize">
+          <div class="tres">
+            <div class="titulo">
+              <img src="img/01.png" class="img1">
+              <h1>Premium</h1>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur.
+            </p>
+            <div>
+                <img src="img/01.png" class="rend-img">
+            </div>
+            <p><a href="#">See More</a></p>
+          </div>
+        </div>
+
+        <div class="organize web">
+          <div class="tres">
+            <div class="titulo">
+              <img src="img/rh.png" class="img1">
+              <h1>Basico</h1>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur.
+            </p>
+            <div>
+              <img src="img/rh.png" class="rend-img">
+            </div>
+            <p><a href="#">See More</a></p>
+          </div>
+        </div>
+
+      </div>
+      <!-- END MIDDLE -->
+
+      <!-- BANNER 2 -->
+      <div id="banner2">
+        <div id="bb">
+          <div id="black">
+            <h3>Registro de dominio</h3>
+            <div id="lgrad">
+              <form method="post"> 
+  WWW.<input type="text" name="dominio" class="btn btn-sucess" size="100%"> 
+<input type="submit" class="btn btn-sobre" name="enviar" value="processar"> Processar <i class="fa fa-search"></i></button>
+</form>
+
+  <?php
+  $dominio=$_POST['dominio'];
+if ($dominio != '') {
+    $ipdo = "http://registro.br/cgi-bin/nicbr/whois?qr=$dominio"; 
+    $ipdo_linha = file($ipdo); 
+    $linha = ($ipdo_linha[47]); 
+    $dom = strtoupper($dominio); 
+    $inexist = "inexistente"; 
+    $link = "http://registro.br"; 
+
+    if (strpos($linha, $dom) && !strpos($linha, $inexist)) {
+        $abc = 67; 
+        echo "O domínio $dominio já está registrado";
+    } else {
+        $abc = 47;
+        echo "O domínio $dominio ainda não está registrado";
+    }
+
+    echo '<br><br>Informações Gerais do Dominio:'.$dom.'<br> 
+<br>---------------------------------------------------------<br><br> 
+<pre>'; 
+
+    for ($i = 47;$i <= $abc; $i++) {
+        $preliminar = str_replace("/cgi-bin", "$link/cgi-bin",$ipdo_linha[$i]); 
+        echo  str_replace("/faq/", "$link/faq/", $preliminar ); 
+    } 
+    echo '
+</pre> 
+--------------------------------------------------------------<br>';
+}
+?> 
+            </div>
+            <h4>TEMPOR INCIDIDUNT UT LABORE</h4>
+            <div id="lgrad">
+              <p class="gradient"></p>
+              <p class="gradient"></p>
+              <p class="gradient"></p>
+              <p class="gradient"></p>
+              <p class="gradient"></p>
+            </div>
+            <p class="last-p">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco <!-- laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. -->
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- END BANNER 2 -->
+
+      <!-- LATEST PROJECT -->
+      <div id="latestproj">
+        <h1>LATEST PROJECT</h1>
+        <div id="lgrad">
+          <p class="gradient"></p>
+          <p class="gradient"></p>
+          <p class="gradient"></p>
+          <p class="gradient"></p>
+          <p class="gradient"></p>
+          <p class="gradient"></p><!--
+          <p class="gradient"></p> 
+          <p class="gradient"></p>
+          <p class="gradient"></p>
+          <p class="gradient"></p> -->
+        </div>
+      </div>
+      <!-- END LATEST PROJECT -->
+
+      <div class="slideshow-container">
+
+        <div class="mySlides fade">
+          <img src="img/6.jpg" style="width:100%; height: 200px;">
+        </div>
+
+        <div class="mySlides fade video">
+          <video width="100%" height="200px" autoplay muted>
+            <source src="vds/v1.mp4" type="video/mp4">
+          </video>
+        </div>
+
+        <div class="mySlides fade">
+          <img src="img/1.jpg" style="width: 100%; height: 200px;">
+        </div>
+
+      </div>
+      <br>
+
+      <div style="text-align:center">
+        <span class="dot"></span> 
+        <span class="dot"></span> 
+        <span class="dot"></span> 
+      </div>
+
+    </div>
+    <!-- END BOX -->
+
+    <!-- FOOTER -->
+    <div id="footer">
+        <div id="fcontent">
+          <p><a href="#">Hospegagem web</a> | <a href="#">Registro de Dominios</a> | <a href="#"> Servidores Dedicaddos</a></p>
+          <h5>SOCIAL MEDIA</h5>
+          <div id="smidea">
+            <a href="#"><img src="img/fb.png"></a>
+            <a href="#"><img src="img/pi.png"></a>
+            <a href="#"><img src="img/li.png"></a>
+            <a href="#"><img src="img/yt.png"></a>
+            <a href="#"><img src="img/em.png"></a>
+            <a href="#"><img src="img/gh.png"></a>
+          </div>
+          <p class="dois">&copy;Desenvolvido por <a href="http:myinfinit.com.br">MyInfinit</a> 2020</p>
+        </div>
+      </div>
+      <!-- END FOOTER -->
+
+  </div>
+  <!-- END CONTAINER -->
+  <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY  -->
+    <script src="assets/plugins/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP CORE SCRIPT   -->
+    <script src="assets/plugins/bootstrap.min.js"></script>  
+    <!-- CUSTOM SCRIPTS -->
+    <script src="assets/js/custom.js"></script>
+  <script type="text/javascript" src="js/jquery/jquery-3.3.1.js"></script>
+  <script type="text/javascript" src="js/pvw.js"></script>
+
+</body>
+</html>
